33294d9c3c99bb22c63a0b70f044437c3f729af0
Hezziac <hezziac@gmail.com>
2026-01-01 14:43:41 -0600

    Fix: Bucket Names

--- src/components/EditPostModal.tsx
+++ src/components/EditPostModal.tsx
@@ -93,7 +93,7 @@ export const EditPostModal = ({ isOpen, onClose, postId, initialTitle, initialCo
       }
 
       const { error: postUpdateError } = await supabase
-        .from("post-images")
+        .from("post")
         .update({ 
           title, 
           content, 
